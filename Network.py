0b58b7c62acaf1e9dfa22a407bc87d4aebf3b582
bartolo1024 <bartolo1024@bitbucket.org>
2018-01-03 12:30:41 +0100

    sth works

--- Network.py
+++ Network.py
@@ -1,37 +1,37 @@
 import tensorflow as tf
 import TrainInput as ti
 import matplotlib.pyplot as plt
-import numpy as np
 
-BATCH = 5000
+BATCH = 2500
 DISPLAY_STEP = 1
 
 # neural network parameters
-number_of_features_hidden_a = 25  #
+number_of_features_hidden_a = 27  #
 number_of_features_hidden_b = 20  #
 number_of_features_hidden_c = 15  #
-#number_of_features_hidden_d = 27  #
-#number_of_features_hidden_e = 35  #
+number_of_features_hidden_d = 10  #
+number_of_features_hidden_e = 10  #
 
 number_of_sensors = 27  #
 number_of_efectors = 3  #
 
 x = tf.placeholder(tf.float32, [None, number_of_sensors], name='state')
 y_ = tf.placeholder(tf.float32, [None, number_of_efectors], name='action')
+target_steering, target_acceleration, target_brake = tf.split(y_, [1, 1, 1], 1)
 
 #
 W1 = tf.Variable(tf.truncated_normal([number_of_sensors, number_of_features_hidden_a], stddev=0.1), name="W1")
 W2 = tf.Variable(tf.truncated_normal([number_of_features_hidden_a, number_of_features_hidden_b], stddev=0.1), name="W2")
 W3 = tf.Variable(tf.truncated_normal([number_of_features_hidden_b, number_of_features_hidden_c], stddev=0.1), name="W3")
-# W4 = tf.Variable(tf.truncated_normal([number_of_features_hidden_c, number_of_features_hidden_d], stddev=0.1), name="W4")
-# W5 = tf.Variable(tf.truncated_normal([number_of_features_hidden_d, number_of_features_hidden_e], stddev=0.1), name="W5")
-W6 = tf.Variable(tf.truncated_normal([number_of_features_hidden_c, number_of_efectors], stddev=0.1), name="W6")
+W4 = tf.Variable(tf.truncated_normal([number_of_features_hidden_c, number_of_features_hidden_d], stddev=0.1), name="W4")
+W5 = tf.Variable(tf.truncated_normal([number_of_features_hidden_d, number_of_features_hidden_e], stddev=0.1), name="W5")
+W6 = tf.Variable(tf.truncated_normal([number_of_features_hidden_e, number_of_efectors], stddev=0.1), name="W6")
 #
 b1 = tf.Variable(tf.zeros(number_of_features_hidden_a), name="b1")
 b2 = tf.Variable(tf.zeros(number_of_features_hidden_b), name="b2")
 b3 = tf.Variable(tf.zeros(number_of_features_hidden_c), name="b3")
-# b4 = tf.Variable(tf.zeros(number_of_features_hidden_d), name="b4")
-# b5 = tf.Variable(tf.zeros(number_of_features_hidden_e), name="b5")
+b4 = tf.Variable(tf.zeros(number_of_features_hidden_d), name="b4")
+b5 = tf.Variable(tf.zeros(number_of_features_hidden_e), name="b5")
 b6 = tf.Variable(tf.zeros(number_of_efectors), name="b6")
 
 # visualization
@@ -45,48 +45,40 @@ speedX, speedY, speedZ, rpm, wheelSpinVel, track = tf.split(x, [1, 1, 1, 1, 4, 1
 normalized_speed_x = tf.divide(speedX, 500)
 normalized_speed_y = tf.divide(speedY, 500)
 normalized_speed_z = tf.divide(speedZ, 500)
-rpm_normalized = tf.divide(rpm, 10000)
+rpm_normalized = tf.divide(rpm, 100)
 wheel_spin_velocity_normalized = tf.divide(wheelSpinVel, 1000)
 track_normalized = track
 normalized_input = tf.concat(
     [normalized_speed_x, normalized_speed_y, normalized_speed_z, rpm_normalized, wheel_spin_velocity_normalized, track],
     1)
-input_layer = tf.nn.tanh(tf.add(tf.matmul(normalized_input, W1), b1))
+input_layer = tf.add(tf.matmul(normalized_input, W1), b1)
 
 hidden_layer_a = tf.nn.relu(tf.add(tf.matmul(input_layer, W2), b2))
 hidden_layer_b = tf.nn.relu(tf.add(tf.matmul(hidden_layer_a, W3), b3))
-#hidden_layer_c = tf.nn.relu(tf.add(tf.matmul(hidden_layer_b, W4), b4))
-#hidden_layer_d = tf.nn.relu(tf.add(tf.matmul(hidden_layer_c, W5), b5))
+hidden_layer_c = tf.nn.relu(tf.add(tf.matmul(hidden_layer_b, W4), b4))
+hidden_layer_d = tf.nn.relu(tf.add(tf.matmul(hidden_layer_c, W5), b5))
 
-unnormalized_output = tf.add(tf.matmul(hidden_layer_b, W6), b6)
+unnormalized_output = tf.add(tf.matmul(hidden_layer_d, W6), b6)
 steering, acceleration, brake = tf.split(unnormalized_output, [1, 1, 1], 1)
 steering_normalized = tf.tanh(steering)  # steering values e(-1, 1)
 acceleration_normalized = tf.sigmoid(acceleration)  # acceleration values e(0, 1)
 brake_normalized = tf.sigmoid(brake)  # acceleration values e(0, 1)
 
-#add random noise
-# def ornstein_uhlenbeck_function(x, mu, theta, sigma):
-#     return tf.add(tf.multiply(theta, tf.add(mu, -x)), tf.multiply(sigma, tf.random_normal(tf.shape(x))))
-#
-# steering_normalized = tf.add(steering_normalized, ornstein_uhlenbeck_function(steering_normalized, 0.0, 0.60, 0.30))
-# acceleration_normalized = tf.add(acceleration_normalized, ornstein_uhlenbeck_function(acceleration_normalized, 0.5, 1.00, 0.10))
-# brake_normalized = tf.add(brake_normalized, ornstein_uhlenbeck_function(brake_normalized, -0.1, 1.00, 0.05))
-
 y = tf.concat([steering_normalized, acceleration_normalized, brake_normalized], 1)
 print(tf.shape(y))
-cross_entropy = tf.reduce_mean(tf.reduce_sum(tf.pow(tf.subtract(y_, y), 2)))
+cross_entropy = tf.reduce_sum(tf.add(tf.add(tf.pow(tf.multiply(tf.subtract(steering_normalized, target_steering), 10), 4),
+                                                    tf.multiply(tf.abs(tf.subtract(acceleration_normalized, target_acceleration)), 1)),
+                                                    tf.multiply(tf.abs(tf.subtract(brake_normalized, target_brake)), 1)))
 
-train_step = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(0.005).minimize(cross_entropy)
 
 init = tf.global_variables_initializer()
 
 train_accurency = list()
 test_accurency = list()
 
-
-
 train_input = ti.TrainInput()
-number_of_iterations = int(train_input.get_train_data_count() / BATCH)
+number_of_iterations = int(train_input.get_train_data_count() / BATCH) * 15
 with tf.Session() as sess:
     sess.run(init)
 
--- TrainInput.py
+++ TrainInput.py
@@ -2,75 +2,42 @@ import h5py as h5
 import numpy as np
 
 class TrainInput(object):
-    def __init__(self, filename = 'stateactionfile.h5', DISPLAY_STEP = 100):
+    def __init__(self, filename = 'alldata.h5', DISPLAY_STEP = 100):
         self.file = h5.File(filename, 'r')
         self.DISPLAY_STEP = DISPLAY_STEP
-        self.dataset_index = 0
 
-        data_max_index = self.file.attrs['data_count'] - 1
-        if data_max_index % 2 == 0:
-            self.max_train_dataset_index = int(data_max_index / 2) - 1
-        else:
-            self.max_train_dataset_index = int(data_max_index / 2)
+        self.data = np.array(self.file.get('sa'))
+        self.data = np.delete(self.data, 2, 1)
+        np.random.shuffle(self.data)
+        self.max_train_index = int(self.data.shape[0]/2) - 1
 
-        self.test_dataset_index = self.max_train_dataset_index + 1
-        self.max_test_dataset_index = data_max_index
-
-        self.load_next_dataset()
-        self.load_next_test_dataset()
-        self.data_counter = 0
+        self.train_data_index = 0
+        self.test_data_index = int(self.data.shape[0]/2)
 
     def get_next_batch(self, batch = 100):
-        state = self.train_data_numpy[self.data_counter: self.data_counter + batch, 3: 30]
-        action = self.train_data_numpy[self.data_counter: self.data_counter + batch, 0: 3]
+        state = self.data[self.train_data_index: self.train_data_index + batch, 3: 30]
+        action = self.data[self.train_data_index: self.train_data_index + batch, 0: 3]
 
-        self.data_counter += batch
-        if self.data_counter >= self.train_data_size:
-            self.load_next_dataset()
+        self.train_data_index += batch
 
-        return np.fliplr(state), np.fliplr(action)
+        if self.train_data_index > self.max_train_index:
+            self.train_data_index = 0
+
+        return state, action
 
     def get_next_test_data(self, batch = 100):
-        state = self.test_data_numpy[self.test_data_counter: self.test_data_counter + batch - 1, 3: 30]
-        action = self.test_data_numpy[self.test_data_counter: self.test_data_counter + batch - 1, 0: 3]
+        state = self.data[self.test_data_index: self.test_data_index + batch - 1, 3: 30]
+        action = self.data[self.test_data_index: self.test_data_index + batch - 1, 0: 3]
 
-        self.test_data_counter += batch
-        if self.test_data_counter >= self.test_data_size:
-            self.load_next_test_dataset()
+        self.test_data_index += batch
 
-        return np.fliplr(state), np.fliplr(action)
+        if self.test_data_index > self.data.shape[0] - 1:
+            self.test_data_index = int(self.data.shape[0]/2)
+
+        return state, action
 
     def get_train_data_count(self):
-        return min((self.max_train_dataset_index + 1) * self.train_data_size, (self.max_test_dataset_index - self.max_train_dataset_index) * self.train_data_size) - 1
-
-    def load_next_dataset(self):
-        self.train_data = self.file.get('sa' + str(self.dataset_index))
-        self.train_data_numpy = np.array(self.train_data)
-        self.train_data_size = self.train_data_numpy.shape[0]
-
-        self.train_data_numpy = np.delete(self.train_data_numpy, 2, 1)
-
-        self.data_counter = 0
-        self.dataset_index += 1
-
-        print(str(self.dataset_index) + 'th dataset loaded')
-        if self.dataset_index == self.max_train_dataset_index:
-            self.dataset_index = 0
-            print('Last dataset loaded')
-
-    def load_next_test_dataset(self):
-        self.test_data = self.file.get('sa' + str(self.test_dataset_index))
-        self.test_data_numpy = np.array(self.test_data)
-        self.test_data_size = self.test_data_numpy.shape[0]
-
-        self.test_data_numpy = np.delete(self.test_data_numpy, 2, 1)
-
-        self.test_data_counter = 0
-        self.test_dataset_index += 1
-
-        if self.test_dataset_index == self.max_test_dataset_index:
-            self.test_dataset_index = 0
-            print('Last test dataset loaded')
+        return self.max_train_index + 1
 
     def close(self):
         self.file.close()
